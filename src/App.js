09a931ad4042f79046a8a64c65e45cad7fe52d67
randomindianguy <sidharth.sundram@gmail.com>
2022-04-06 12:55:29 +0530

    created skeleton of home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 function App() {
   return (
     <div>
-      Phone Directory
+      {/* <div>
+        Phone Directory
+      </div>
+      <button>Add</button>
+      <div>
+        <span> Name </span>
+        <br />
+        <span> Phone </span>
+      </div> */}
+    <label htmlFor="name">Name: </label>
+    <input id="name" type="text" placeholder="Type Here" />
     </div>
   );
 }
