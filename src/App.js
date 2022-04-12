495329e02f7586a9a313465d656c427e284b7ed1
randomindianguy <sidharth.sundram@gmail.com>
2022-04-12 16:25:18 +0530

    created header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-function App() {
-  return (
-    <div>
-      {/* <div>
-        Phone Directory
-      </div>
-      <button>Add</button>
-      <div>
-        <span> Name </span>
-        <br />
-        <span> Phone </span>
-      </div> */}
-    <label htmlFor="name">Name: </label>
-    <input id="name" type="text" placeholder="Type Here" />
-    </div>
+import React from 'react';
+
+import React, { Component } from 'react';
+
+class Header extends Component {
+   render() {
+       return (
+           <div className="header">
+               Phone Directory
+           </div>
+       )
+   }
+}
+    // <div id="module">
+    //     <p>ReactJS</p>
+    // </div>
+
+    // React.createElement("div", {id: "module"}, 
+    //   React.createElement("p",null,"ReactJS")
+    // )
   );
 }
 
