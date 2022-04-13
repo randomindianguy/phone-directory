aba9241b11d8842ed72c0059773add8e81e669d1
randomindianguy <sidharth.sundram@gmail.com>
2022-04-13 14:34:02 +0530

    Styled App (Home Page)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import Header from './Header';
-
+import './App.css';
 function App() {
   // let x = 2;
   // let y = 3;
   return (
     <div>
       <Header />
-      <button>Add</button>
-      <div>
-        <span> Name </span>
-        <br />
-        <span> Phone </span>
+      <div className='component-body-container'>
+        <button className='custom-btn add-btn'>Add</button>
+
+        <div className='grid-container heading-container'>
+          <span className='grid-item name-heading'>Name</span>
+          <span className='grid-item phone-heading'>Name</span>
+        </div>
       </div>
     {/* <label htmlFor="name">Name: </label>
     <input id="name" type="text" defaultValue={x+y} /> */}
