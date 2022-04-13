0331c6f2a3dc6ec56144c4e9ed3706744172d5ee
randomindianguy <sidharth.sundram@gmail.com>
2022-04-13 15:34:04 +0530

    Added 'Delete' button

--- src/App.js
+++ src/App.js
@@ -31,9 +31,15 @@ function App() {
             return <div ley={sub.id} className='grid-container'>
             <span className='grid-item'>{sub.name}</span>
             <span className='grid-item'>{sub.phone}</span>
+            <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+            </span>
             </div>
           })
         }
+      
+      
+      
 
         
       </div>
